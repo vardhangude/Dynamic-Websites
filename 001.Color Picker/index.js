4e75ed0fb311103a80cfad13f38e2ba2697674f4
Vardhan Gude <81156630+vardhangude@users.noreply.github.com>
2024-07-05 23:29:25 +0530

    001 - Color Picker

---
+++ 001.Color Picker/index.js
@@ -0,0 +1,81 @@
+let colorPickerContainerElement = document.getElementById("colorPickerContainer")
+let selectedColorHexCodeElement = document.getElementById("selectedColorHexCode");
+
+function changeBgToGreyAndUpdateText() {
+    colorPickerContainerElement.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCodeElement.textContent = "#e0e0e0";
+}
+
+function changeBgToGreenAndUpdateText() {
+    colorPickerContainerElement.style.backgroundColor = "#6fcf97";
+    selectedColorHexCodeElement.textContent = "#6fcf97";
+
+}
+
+function changeBgToBlueAndUpdateText() {
+    colorPickerContainerElement.style.backgroundColor = "#56ccf2";
+    selectedColorHexCodeElement.textContent = "#56ccf2";
+}
+
+function changeBgToPurpleAndUpdateText() {
+    colorPickerContainerElement.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCodeElement.textContent = "#bb6bd9";
+
+}
+
+
+
+// function f1() {
+//     document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+//     document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+// }
+
+// function f2() {
+//     document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+//     document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+
+// }
+
+// function f3() {
+//     document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+//     document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+// }
+
+// function f4() {
+//     document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+//     document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let background_color = document.getElementById("colorPickerContainer");
+// let changeHexcode = document.getElementById("selectedColorHexCode");
+
+// function changeBgToGreyAndUpdateText() {
+//     background_color.style.backgroundColor = "#e0e0e0";
+//     // button1.document.getElementById.style.backgroundColor = "#e0e0e0";
+//     changeHexcode.textContent = "#e0e0e0";
+// }
+
+// function changeBgToGreenAndUpdateText() {
+//     background_color.style.backgroundColor = "#6fcf97";
+//     changeHexcode.textContent = "#6fcf97";
+// }
+
+// function changeBgToBlueAndUpdateText() {
+//     background_color.style.backgroundColor = "#56ccf2";
+//     changeHexcode.textContent = "#56ccf2";
+// }
+
+// function changeBgToPurpleAndUpdateText() {
+//     document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+//     document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+// }
