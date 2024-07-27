cbbb5cf28c7556d79112777dea4739bb7c1b6219
Vardhan Gude <81156630+vardhangude@users.noreply.github.com>
2024-07-27 22:42:29 +0530

    Movie Reviews - 13
    <!-- The Movie Reviews section offers critiques, ratings, and summaries of films, helping users make informed decisions about what to watch. -->

---
+++ 013. Movie Reviews/Readme.md
@@ -0,0 +1,21 @@
+## Movie Reviews
+
+In this assignment, let's build a Dynamic Movie Reviews.
+
+**Refer to the below image.**
+
+The following images illustrate all device sizes, from extra small to extra large.
+
+- ![alt text](movie-reviews-v1.gif)
+
+
+**Resources**
+
+- CSS Colors used:
+
+    - `#2d3a35`
+    - `#7b8794`
+    - `#cbd2d9`
+
+- CSS Font families used:
+    - Roboto
---
+++ 013. Movie Reviews/index.js
@@ -0,0 +1,29 @@
+let reviewsContainerEl = document.getElementById("reviewsContainer");
+let titleInputEl = document.getElementById("titleInput");
+let reviewTextareaEl = document.getElementById("reviewTextarea");
+
+function onAddReview() {
+    let movieTitle = titleInputEl.value;
+    let movieReview = reviewTextareaEl.value;
+    if (movieTitle === "") {
+        alert("Please enter a movie title");
+        return;
+    }
+
+    let movieTitleEl = document.createElement("h1");
+    movieTitleEl.textContent = "Movie Title: " + movieTitle;
+    movieTitleEl.classList.add("movie-title");
+    reviewsContainerEl.appendChild(movieTitleEl);
+
+    let movieReviewEl = document.createElement("p");
+    movieReviewEl.textContent = "Review: " + movieReview;
+    reviewsContainerEl.appendChild(movieReviewEl);
+
+    let horizontallineEl = document.createElement("hr");
+    reviewsContainerEl.appendChild(horizontallineEl);
+
+
+    titleInputEl.value = "";
+    reviewTextareaEl.value = "";
+
+}
